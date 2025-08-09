e547f3ee609234f04e569a0db0127de0eaaad438
Divyam Singh <divyamsingh1918@gmail.com>
2025-08-09 09:52:11 +0530

    updates

---
+++ interface/packages/components/3d/nft-model.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import * as Three from 'three'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
+
+const scene = new Three.Scene();
+ 
+const NftModel = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NftModel
