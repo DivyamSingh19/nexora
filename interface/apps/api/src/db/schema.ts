643c3739d230fcee70973df2e017502a930dec71
Divyam Singh <divyamsingh1918@gmail.com>
2025-09-14 13:22:47 +0530

    updates

---
+++ interface/apps/api/src/db/schema.ts
@@ -0,0 +1,3 @@
+import { pgTable,pgEnum } from "drizzle-orm/pg-core";
+import { text,uuid,numeric,jsonb } from "drizzle-orm/pg-core";
+
--- interface/apps/api/src/index.ts
+++ interface/apps/api/src/index.ts
@@ -1,9 +1,10 @@
 import { Hono } from 'hono'
-
+import {cors} from "hono/cors"
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+app.use(cors())
 
 export default app
