133acf39f3b10fa3652adf28d8255460c5aa3dc7
Divyam Singh <divyamsingh1918@gmail.com>
2025-09-14 16:20:30 +0530

    updates

--- interface/apps/api/src/controllers/auth.controller.ts
+++ interface/apps/api/src/controllers/auth.controller.ts
@@ -1,1 +1,46 @@
- 
+import { eq } from "drizzle-orm"; 
+import dotenv from "dotenv"
+import { Context } from "hono";
+
+
+
+export const registerUser = async (c:Context) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const registerTrader = async (c:Context) => {
+    try {
+        
+    } catch (error) {
+        return c.json({
+            success:false,
+            message:"Trader registration failed"
+        },500)
+    }
+}
+
+export const loginUser = async (c:Context) => {
+    try {
+        
+    } catch (error) {
+        return c.json({
+            success:false,
+            message:"User login failed"
+        })
+    }
+}
+
+export const loginTrader = async (c:Context) => {
+    try {
+        
+    } catch (error) {
+        return c.json({
+            success:false,
+            message:"Failed to login trader"
+        })
+    }
+}
--- interface/apps/api/src/db/schema.ts
+++ interface/apps/api/src/db/schema.ts
@@ -1,3 +1,9 @@
 import { pgTable,pgEnum } from "drizzle-orm/pg-core";
 import { text,uuid,numeric,jsonb } from "drizzle-orm/pg-core";
 
+
+
+export const users= pgTable('users',{
+    id:uuid('id').primaryKey().defaultRandom().notNull(),
+    
+})
